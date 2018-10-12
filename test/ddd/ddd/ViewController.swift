10a9322b4a77131142ab1f513ff3e6708f35b088
yangs <yangshuspoil@163.com>
2018-10-12 16:35:06 +0800

    d

--- test/ddd/ddd/ViewController.swift
+++ test/ddd/ddd/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
+        UIApplication.shared.isStatusBarHidden = true
+        
         print(UIApplication.shared.windows.first?.safeAreaInsets)
     }
 }
